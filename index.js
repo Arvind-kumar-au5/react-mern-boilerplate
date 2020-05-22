8304e2e836e655c1dfed5d1ef73a2661f2def356
avigehlot05@gmail.com <avigehlot05@gmail.com>
2020-05-22 15:49:47 +0530

    config

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 // Entry point
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://mentor:Pm1IfYvuNBkmLR8X@cluster0-4o6cx.mongodb.net/test?retryWrites=true&w=majority',{
+const {User} = require('./modal/user')
+const config = require('./config/key')
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true
 }).then(()=>{
     console.log("DB CONNECTED")
 }).catch(err=>console.log(err))
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(cookieParser());
+
+
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
 
+app.post('/api/v1/users/register',(req,res)=>{
+    const user = new User(req.body)
+    user.save((err,userData)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
 
-app.listen(5000)
+
+app.listen(5001)
